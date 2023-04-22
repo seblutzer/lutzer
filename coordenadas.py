ae6f1575da58dba1633c730c9e6615261c8fd890
seblutzer <seblutzer@gmail.com>
2023-04-22 08:16:07 +0000

    Adicionado o pacote lutzer

---
+++ coordenadas.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def get_coordinates(char):
+    coordinates = []
+    if char == 'A':
+        coordinates = [(1,1), (1,2), (1,3), (1,4), (2,5), (3,4), (3,3), (3,2), (3,1), (3,3), (2,3), (1,3)]
+    elif char == 'B':
+        coordinates = [(1,1), (1,2), (1,3), (1,4), (1,5), (2,5), (3,4), (2,3), (1,3), (2,3), (3,2), (2,1), (1,1)]
+    elif char == 'C':
+        coordinates = [(3,1), (2,1), (1,2), (1,3), (1,4), (2,5), (3,5)]
+    elif char == 'D':
+        coordinates = [(1,1), (1,2), (1,3), (1,4), (1,5), (2,5), (3,4), (3,3), (3,2), (2,1), (1,1)]
+    elif char == 'E':
+        coordinates = [(3,1), (2,1), (1,1), (1,2), (1,3), (2,3), (1,3), (1,4), (1,5), (2,5), (3,5)]
+    elif char == 'F':
+        coordinates = [(1,1), (1,2), (1,3), (2,3), (1,3), (1,4), (1,5), (2,5), (3,5)]
+    elif char == 'G':
+        coordinates = [(2,3), (3,3), (3,2), (3,1), (2,1), (1,2), (1,3), (1,4), (2,5), (3,5)]
+    elif char == 'H':
+        coordinates = [(1,1), (1,2), (1,3), (1,4), (1,5), (1,3), (2,3), (3,3), (3,4), (3,5), (3,2), (3,1)]
+    elif char == 'I':
+        coordinates = [(2,1), (2,2), (2,3), (2,4), (2,5)]
+    elif char == 'J':
+        coordinates = [(1,3), (1,2), (2,1), (3,2), (3,3), (3,4), (3,5)]
+    elif char == 'K':
+        coordinates = [(1,1), (1,2), (1,3), (1,4), (1,5), (1,3), (2,4), (3,5), (1,3), (2,2), (3,1)]
+    elif char == 'L':
+        coordinates = [(3,1), (2,1), (1,1), (1,2), (1,3), (1,4), (1,5)]
+    elif char == 'M':
+        coordinates = [(1,1), (1,2), (1,3), (1,4), (1,5), (2,2), (3,5), (3,4), (3,3), (3,2), (3,1)]
+    elif char == 'N':
+        coordinates = [(1,1), (1,2), (1,3), (1,4), (1,5), (2,3), (3,1), (3,2), (3,3), (3,4), (3,5)]
+    elif char == 'O':
+        coordinates = [(3,2), (2,1), (1,2), (1,3), (1,4), (2,5), (3,4), (3,3), (3,2)]
+    elif char == 'P':
+        coordinates = [(1,1), (1,2), (1,3), (1,4), (1,5), (2,5), (3,4), (2,3), (1,3)]
+    elif char == 'Q':
+        coordinates = [(2,3), (3,2), (2,1), (1,2), (1,3), (1,4), (2,5), (3,4), (3,3), (3,2), (3,1)]
+    elif char == 'R':
+        coordinates = [(1,1), (1,2), (1,3), (1,4), (1,5), (2,5), (3,4), (2,3), (1,3), (2,3), (3,1)]
+    elif char == 'S':
+        coordinates = [(1,1), (2,1), (3,2), (2,3), (1,4), (2,5), (3,5)]
+    elif char == 'T':
+        coordinates = [(2,1), (2,2), (2,3), (2,4), (2,5), (1,5), (3,5)]
+    elif char == 'U':
+        coordinates = [(3,5), (3,4), (3,3), (3,2), (2,1), (1,2), (1,3), (1,4), (1,5)]
+    elif char == 'V':
+        coordinates = [(3,5), (2,1), (1,5)]
+    elif char == 'W':
+        coordinates = [(1,5), (1,4), (1,3), (1,2), (1,1), (2,4), (3,1), (3,2), (3,3), (3,4), (3,5)]
+    elif char == 'X':
+        coordinates = [(1,1), (2,3), (3,5), (2,3), (1,5), (3,1)]
+    elif char == 'Y':
+        coordinates = [(2,1), (2,3), (3,5), (2,3), (1,5)]
+    elif char == 'Z':
+        coordinates = [(3,1), (2,1), (1,1), (2,3), (3,5), (2,5), (1,5)]
+    elif char == '0':
+        coordinates = [(3,2), (2,1), (1,2), (1,3), (1,4), (2,5), (3,4), (3,3), (3,2)]
+    elif char == '1':
+        coordinates = [(2,1), (2,2), (2,3), (2,4), (2,5), (1,4)]
+    elif char == '2':
+        coordinates = [(3,1), (2,1), (1,2), (2,3), (3,4), (2,5), (1,5)]
+    elif char == '3':
+        coordinates = [(1,1), (2,1), (3,1), (3,2), (3,3), (2,3), (3,3), (3,4), (3,5), (2,5), (1,5)]
+    elif char == '4':
+        coordinates = [(3,1), (3,2), (3,3), (3,4), (3,5), (1,3), (2,3), (3,3)]
+    elif char == '5':
+        coordinates = [(1,1), (2,1), (3,2), (2,3), (1,3), (1,4), (1,5), (2,5), (3,5)]
+    elif char == '6':
+        coordinates = [(1,2), (2,3), (3,2), (2,1), (1,2), (1,3), (1,4), (2,5), (3,5)]
+    elif char == '7':
+        coordinates = [(1,1), (2,3), (3,5), (2,5), (1,5)]
+    elif char == '8':
+        coordinates = [(3,2), (2,1), (1,2), (2,3), (1,4), (2,5), (3,4), (2,3), (3,2)]
+    elif char == '9':
+        coordinates = [(1,1), (2,1), (3,2), (3,3), (3,4), (2,5), (1,4), (2,3), (3,3)]
+    elif char == ' ':
+        coordinates = []
+    else:
+        coordinates = []
+
+    #Return the list of coordinates
+    return coordinates
+
+def get_word_coordinates(word):
+    word = word.upper()
+    coordinates_df = pd.DataFrame(columns=['letra', 'x', 'y'])
+    letter_count = {}
+    for i, char in enumerate(word):
+        if char not in letter_count:
+            letter_count[char] = 1
+        else:
+            letter_count[char] += 1
+        coordinates = get_coordinates(char)
+        for coord in coordinates:
+            x = coord[0] + i*4
+            y = coord[1]
+            if letter_count[char] > 1:
+                new_row = {'letra': f"{char}{letter_count[char]}", 'x': x, 'y': y}
+            else:
+                new_row = {'letra': char, 'x': x, 'y': y}
+            coordinates_df = pd.concat([coordinates_df, pd.DataFrame(new_row,index=[0])],ignore_index=True)
+    return coordinates_df
---
+++ grafico.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from .coordenadas import get_word_coordinates
+
+
+class Word:
+    def __init__(self, word):
+        self.word = word.upper()
+        self.df = get_word_coordinates(self.word)
+
+    def plot(self):
+        num_chars = len(self.word)
+        width = num_chars * 1.5
+        height = 3
+
+        fig = plt.figure(figsize=(width, height))
+        ax = fig.add_subplot(111)
+
+        for _, group in self.df.groupby('letra'):
+            group = group.reset_index(drop=True)
+            for i in range(len(group)-1):
+                ax.plot([group.loc[i, 'x'], group.loc[i+1, 'x']],
+                        [group.loc[i, 'y'], group.loc[i+1, 'y']],
+                        color='blue')
+
+        for _, group in self.df.groupby('letra'):
+            group = group.reset_index(drop=True)
+            ax.scatter(group['x'], group['y'], color='black')
+
+        plt.show()
